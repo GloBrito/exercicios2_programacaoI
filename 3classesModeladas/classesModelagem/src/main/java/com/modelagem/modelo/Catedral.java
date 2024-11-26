4c076dcfc4947797cd422dc2ef482e3dd53c981a
Glória Brito <gloriafbrito@gmail.com>
2024-11-26 00:30:02 -0300

    feat: acrescenta classe Catedral

--- 3classesModeladas/classesModelagem/src/main/java/com/modelagem/modelo/Catedral.java
+++ 3classesModeladas/classesModelagem/src/main/java/com/modelagem/modelo/Catedral.java
@@ -1,4 +1,53 @@
 package com.modelagem.modelo;
 
 public class Catedral {
+    private String nome;
+    private String localizacao;
+    private int capacidade; //em número de pessoas
+
+    //construtor
+    public Catedral(String nome, String localizacao, int capacidade) {
+        this.nome = nome;
+        this.localizacao = localizacao;
+        this.capacidade = capacidade;
+    }
+
+    //métodos
+    public void exibirInformacoes() {
+        System.out.println("Catedral: " + nome + ", Localização: " + localizacao + ", Capacidade: " + capacidade + " pessoas.");
+    }
+
+    public void tocarSinos() {
+        System.out.println("Os sinos da catedral estão tocando!");
+    }
+
+    public boolean aberto() {
+        //catedral fica sempre aberta
+        return true;
+    }
+
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
 }
