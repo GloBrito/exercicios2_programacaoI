3fb80da8ac07d90549602d90864799fd123e710e
Glória Brito <gloriafbrito@gmail.com>
2024-11-26 00:29:28 -0300

    feat: acrescenta classe Bolo

--- 3classesModeladas/classesModelagem/src/main/java/com/modelagem/modelo/Bolo.java
+++ 3classesModeladas/classesModelagem/src/main/java/com/modelagem/modelo/Bolo.java
@@ -1,4 +1,27 @@
 package com.modelagem.modelo;
 
 public class Bolo {
+    private String sabor;
+    private int tamanho;
+    private double peso;
+
+    public Bolo(String sabor, int tamanho, double peso) {
+        this.sabor = sabor;
+        this.tamanho = tamanho;
+        this.peso = peso;
+    }
+
+    public boolean assado() {
+        return true;
+    }
+
+    public void decorar(String estilo) {
+        System.out.println("O bolo foi decorado no estilo: " + estilo);
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Sabor: " + sabor);
+        System.out.println("Tamanho: " + tamanho + " cm");
+        System.out.println("Peso: " + peso + " kg");
+    }
 }
