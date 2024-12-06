cfa771ded7df55dd3359c095200c6b0b26161958
Glória Brito <gloriafbrito@gmail.com>
2024-12-06 01:32:21 -0300

    fix: corrige metodos

--- 3classesModeladas/classesModelagem/src/main/java/com/modelagem/Main.java
+++ 3classesModeladas/classesModelagem/src/main/java/com/modelagem/Main.java
@@ -5,43 +5,35 @@ import com.modelagem.modelo.Caneta;
 import com.modelagem.modelo.Catedral;
 
 public class Main {
+
     public static void main(String[] args) {
-        //chama metodo para testar a classe Bolo
-        testarBolo();
+        //cria objetos
+        Bolo bolo = new Bolo("Chocolate", 20, 1.5);
+        Caneta caneta = new Caneta("azul", "Bic", 0.7);
+        Catedral catedral = new Catedral("Notre-Dame", "Paris", 5000);
 
-        //chama metodo para testar a classe Caneta
-        testarCaneta();
-
-        //chama metodo para testar a classe Catedral
-        testarCatedral();
+        //teste dos métodos das classes
+        testarBolo(bolo);
+        testarCaneta(caneta);
+        testarCatedral(catedral);
     }
 
     //metodo para testar a classe Bolo
-    public static void testarBolo() {
-        Bolo bolo = new Bolo("Chocolate", 20, 1.5);
-
+    public static void testarBolo(Bolo bolo) {
         bolo.exibirDetalhes();
         bolo.decorar("Festivo");
         System.out.println("Está assado? " + bolo.assado());
     }
 
     //metodo para testar a classe Caneta
-    public static void testarCaneta() {
-        // Cria uma instância de Caneta
-        Caneta minhaCaneta = new Caneta("azul", "Bic", 0.7);
-
-        // Usa os métodos da classe Caneta
-        minhaCaneta.escrever("Olá, como você está?");
-        minhaCaneta.trocarTinta("vermelha");
-        minhaCaneta.verificarNivelTinta();
+    public static void testarCaneta(Caneta caneta) {
+        caneta.escrever("Olá, como você está?");
+        caneta.trocarTinta("vermelha");
+        caneta.verificarNivelTinta();
     }
 
     //metodo para testar a classe Catedral
-    public static void testarCatedral() {
-        // Cria uma instância de Catedral
-        Catedral catedral = new Catedral("Notre-Dame", "Paris", 5000);
-
-        //usa os metodos da classe Catedral
+    public static void testarCatedral(Catedral catedral) {
         catedral.exibirInformacoes();
         catedral.tocarSinos();
         System.out.println("A catedral está aberta? " + catedral.aberto());
