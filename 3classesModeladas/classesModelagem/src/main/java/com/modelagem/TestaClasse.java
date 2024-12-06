088e931779230d4fc401bd487b58d07dc081a866
Glória Brito <gloriafbrito@gmail.com>
2024-12-06 01:33:20 -0300

    feat: insere teste classes

---
+++ 3classesModeladas/classesModelagem/src/main/java/com/modelagem/TestaClasse.java
@@ -0,0 +1,69 @@
+package com.modelagem;
+
+import com.modelagem.modelo.Bolo;
+import com.modelagem.modelo.Caneta;
+import com.modelagem.modelo.Catedral;
+import java.util.Scanner;
+
+public class TestaClasse {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //cria objetos
+        Bolo bolo = new Bolo("Morango", 25, 2.0);
+        Caneta caneta = new Caneta("preta", "Faber-Castell", 0.5);
+        Catedral catedral = new Catedral("Sagrada Família", "Barcelona", 12000);
+
+        int opcao;
+        do {
+            //exibe menu
+            System.out.println("\nMenu:");
+            System.out.println("1. Testar Bolo");
+            System.out.println("2. Testar Caneta");
+            System.out.println("3. Testar Catedral");
+            System.out.println("4. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            //processa opções
+            switch (opcao) {
+                case 1:
+                    testarBolo(bolo);
+                    break;
+                case 2:
+                    testarCaneta(caneta);
+                    break;
+                case 3:
+                    testarCatedral(catedral);
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opcao != 4);
+    }
+
+    //metodo para testar a classe Bolo
+    public static void testarBolo(Bolo bolo) {
+        bolo.exibirDetalhes();
+        bolo.decorar("Elegante");
+        System.out.println("Está assado? " + bolo.assado());
+    }
+
+    //metodo para testar a classe Caneta
+    public static void testarCaneta(Caneta caneta) {
+        caneta.escrever("Testando a caneta...");
+        caneta.trocarTinta("azul");
+        caneta.verificarNivelTinta();
+    }
+
+    //metodo para testar a classe Catedral
+    public static void testarCatedral(Catedral catedral) {
+        catedral.exibirInformacoes();
+        catedral.tocarSinos();
+        System.out.println("A catedral está aberta? " + catedral.aberto());
+    }
+}
