9a6381bff33c3a47e45363b9ebab0b53ea8fe52a
Glória Brito <gloriafbrito@gmail.com>
2024-12-07 12:56:03 -0300

    feat: adiciona classe inseto

--- 3classesModeladas/classesModelagem/src/main/java/com/modelagem/Main.java
+++ 3classesModeladas/classesModelagem/src/main/java/com/modelagem/Main.java
@@ -5,6 +5,7 @@ import com.modelagem.modelo.Caneta;
 import com.modelagem.modelo.Catedral;
 import com.modelagem.modelo.Pintura;
 import com.modelagem.modelo.Lustre;
+import com.modelagem.modelo.Inseto;
 
 
 public class Main {
@@ -16,6 +17,7 @@ public class Main {
         Catedral catedral = new Catedral("Notre-Dame", "Paris", 5000);
         Pintura pintura = new Pintura("branca", "acrílica", 30.0);
         Lustre lustre = new Lustre("vidro", 60, "dourado");
+        Inseto inseto = new Inseto("Borboleta", 2.5, 6);
 
 
         //teste dos métodos das classes
@@ -24,6 +26,7 @@ public class Main {
         testarCatedral(catedral);
         testarPintura(pintura);
         testarLustre(lustre);
+        testarInseto(inseto);
 
 
     }
@@ -61,5 +64,11 @@ public class Main {
         lustre.ajustarBrilho(5);
         lustre.apagar();
     }
-
+    //metodo para testar a classe Inseto
+    public static void testarInseto(Inseto inseto) {
+        System.out.println("\nTestando a classe Inseto:");
+        inseto.mover();
+        inseto.emitirSom();
+        inseto.alimentar("néctar de flores");
+    }
 }
